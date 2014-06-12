38e6c4bfc604cb128dc9a76ab1f0bdcd8aa893e6
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 04:09:02 -0700

    new version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var transformer = require('dat-transformer');
-var tString = transformer('string');
-var tNumber = transformer('number');
+var Conversion = require('transformer-conversion');
+var tString = require('transformer.string');
+var tNumber = require('transformer.number');
 // require any other modules you may need here.
 
-module.exports = transformer.Conversion(tString, tNumber, convert);
+module.exports = Conversion(tString, tNumber, convert);
 
 function convert(str) {
   return parseFloat(str);
